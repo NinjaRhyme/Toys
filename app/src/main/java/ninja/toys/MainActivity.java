dbbb825f19720f762a5fb26c18883350fd608157
NinjaRhyme <NinjaRhyme@gmail.com>
2015-11-01 22:23:04 +0800

    Try volley library

--- app/src/main/java/ninja/toys/MainActivity.java
+++ app/src/main/java/ninja/toys/MainActivity.java
@@ -1,12 +1,24 @@
 package ninja.toys;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -21,6 +33,61 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         m_requestQueue = Volley.newRequestQueue(getApplicationContext());
+
+        // string
+        String stringURL = "http://www.baidu.com";
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, stringURL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        TextView textView = (TextView) findViewById(R.id.textView);
+                        textView.setText(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        TextView textView = (TextView) findViewById(R.id.textView);
+                        textView.setText(error.toString());
+                    }
+                }
+        );
+        m_requestQueue.add(stringRequest);
+
+        // image
+        String imageURL = "http://static.byr.cn/files/imgupload/2013-05-17-23-37-03.jpg";
+        ImageRequest imageRequest = new ImageRequest(imageURL, new Response.Listener<Bitmap>() {
+            @Override
+            public void onResponse(Bitmap response) {
+                ImageView imageView = (ImageView) findViewById(R.id.imageView);
+                imageView.setImageBitmap(response);
+            }
+        }, 0, 0, ImageView.ScaleType.FIT_XY, Bitmap.Config.RGB_565, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                ImageView imageView = (ImageView) findViewById(R.id.imageView);
+                imageView.setImageBitmap(null);
+            }
+        });
+        m_requestQueue.add(imageRequest);
+
+        // json
+        String jsonURL = "";
+        JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.GET, jsonURL, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Toast.makeText(MainActivity.this, response.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this,error.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+        m_requestQueue.add(jsonRequest);
     }
 
     //----------------------------------------------------------------------------------------------------
